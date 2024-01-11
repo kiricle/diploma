3d4b8989dd85a7cea59978aea9c139c64eae012b
Kolotylo Kyrylo <kolot102@gmail.com>
2024-01-11 18:32:23 +0200

    feat(ui): add heading component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import Image from 'next/image';
 import styles from './page.module.scss';
 import { Button } from '@/app/ui/Button/Button';
+import { Heading } from './ui/Heading/Heading';
 
 export default function Home() {
     return (
@@ -8,6 +9,11 @@ export default function Home() {
             <Button appearance="primary">Button</Button>
             <Button appearance="secondary">Button</Button>
             <Button appearance="danger">Button</Button>
+            <Heading level={1}>Heading</Heading>
+            <Heading level={2}>Heading</Heading>
+            <Heading level={3}>Heading</Heading>
+            <Heading level={4}>Heading</Heading>
+            <Heading level={5}>Heading</Heading>
         </main>
     );
 }
