0da30d531df34fedc73a0923fbf93f70b6b07e7f
Kolotylo Kyrylo <kolot102@gmail.com>
2024-01-13 17:39:42 +0200

    fix(animation): the logo size and links animation fixes

--- src/ui/Logo/Logo.tsx
+++ src/ui/Logo/Logo.tsx
@@ -2,5 +2,5 @@ import Image from "next/image";
 import LogoSVG from './logo.svg';
 
 export const Logo = () => {
-    return <Image width={75} height={75} src={LogoSVG} alt="Logo"/>;
+    return <Image width={125} height={100} src={LogoSVG} alt="Logo"/>;
 }; 
