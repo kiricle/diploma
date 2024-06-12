ea70f55fa5e8ca171c6511f56bfe7f6d50c8a3be
Kolotylo Kyrylo <kolot102@gmail.com>
2024-06-12 18:01:11 +0300

    result

--- src/services/tokens.service.ts
+++ src/services/tokens.service.ts
@@ -1,23 +1,25 @@
-import Cookies from 'js-cookie'
+import Cookies from 'js-cookie';
 
 export enum EnumTokens {
-	'ACCESS_TOKEN' = 'accessToken',
-	'REFRESH_TOKEN' = 'refreshToken'
+    'ACCESS_TOKEN' = 'accessToken',
+    'REFRESH_TOKEN' = 'refreshToken',
 }
 
 export const getAccessToken = () => {
-	const accessToken = Cookies.get(EnumTokens.ACCESS_TOKEN)
-	return accessToken || null
-}
+    const accessToken = Cookies.get(EnumTokens.ACCESS_TOKEN);
+    return accessToken || null;
+};
 
 export const saveTokenStorage = (accessToken: string) => {
-	Cookies.set(EnumTokens.ACCESS_TOKEN, accessToken, {
-		domain: 'https://diploma-be-production.up.railway.app/',
-		sameSite: 'strict',
-		expires: 1
-	})
-}
+    const result = Cookies.set(EnumTokens.ACCESS_TOKEN, accessToken, {
+        domain: 'https://diploma-be-production.up.railway.app/',
+        sameSite: 'strict',
+        expires: 1,
+    });
+
+    console.log('res', result);
+};
 
 export const removeFromStorage = () => {
-	Cookies.remove(EnumTokens.ACCESS_TOKEN)
-}
+    Cookies.remove(EnumTokens.ACCESS_TOKEN);
+};
